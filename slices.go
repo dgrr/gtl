850f4028a7ffa90636f4193fe4af2aac8e85d8f9
Dario <dgrripoll@gmail.com>
2021-09-18 19:42:34 +0200

    std comparable

--- slices.go
+++ slices.go
@@ -1,11 +1,7 @@
 package gtl
 
-type Comparable interface {
-	Ordered
-}
-
 // Contains returns whether `vs` contains the element `e`.
-func Contains[T Comparable](vs []T, e T) bool {
+func Contains[T comparable](vs []T, e T) bool {
 	for _, v := range vs {
 		if v == e {
 			return true
